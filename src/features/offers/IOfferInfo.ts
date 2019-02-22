0528eb2186663118c2d7d82f334ecd5220875129
Siddhartha <siddharthac6@gmail.com>
2019-02-22 20:50:56 +0530

    created react-redux setup

---
+++ src/features/offers/IOfferInfo.ts
@@ -0,0 +1,6 @@
+export interface IOfferInfo {
+    name: string;
+    imageSource: string;
+    price: number;
+    currency: string;
+}
---
+++ src/features/offers/IdOfferSortType.ts
@@ -0,0 +1,6 @@
+export const enum IdSortType {
+    byDefault = "default",
+    byName = "name",
+    byPopularity = "popularity",
+    byPrice = "price"
+}
